73e58e330547bcefc751d5fde96b947ada32e493
Brent Jackson <jxnblk@github.com>
2014-11-11 11:36:41 -0500

    Minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var browserify = require('gulp-browserify');
 var basswork = require('gulp-basswork');
+var minifyCss = require('gulp-minify-css');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 
 gulp.task('css', function() {
   gulp.src('./src/css/base.css')
     .pipe(basswork())
+    .pipe(minifyCss())
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('js', function() {
   gulp.src('./src/js/app.js')
     .pipe(browserify())
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('./js'));
 });
 
